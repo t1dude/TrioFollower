16c595758f2c4b41aacb2c843a245d8dd92ebdd1
t1dude <90277542+t1dude@users.noreply.github.com>
2026-09-21 13:43:17 +0200

    Release v0.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.trionsandroid.app"
         minSdk = 31
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.2.4"
+        versionCode = 7
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
